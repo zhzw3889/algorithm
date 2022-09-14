18d27bb24a8992fc4f1aeab0f1d823805e4495d8
zhzw <hitzhzw3889@126.com>
2022-09-14 17:11:53 +0800

    Add: 1、增加插值查找 2、改进二分查找mid表示法

--- searching/searching.go
+++ searching/searching.go
@@ -9,7 +9,31 @@ func BinarySearch(nums []int, target int) int {
 	// 当low=high时只有一个元素，mid=high=low
 	// 下一步如果还不满足target==nums[mid]，则会出现low>high的情况，跳出循环
 	for low <= high {
-		mid = (low + high) / 2
+		// mid = (low + high) / 2
+		// 避免整型溢出
+		mid = low + (high-low)/2
+		if target < nums[mid] {
+			high = mid - 1
+		} else if target > nums[mid] {
+			low = mid + 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
+
+// 插值查找，改进mid取值
+func InterpolationSearch(nums []int, target int) int {
+	N := len(nums)
+	low, high := 0, N-1
+	var mid int
+	for low <= high {
+		// 只有此处改进，mid可能出界
+		mid = int(float64(low) + float64(target-nums[low])/float64(nums[high]-nums[low])*float64(high-low))
+		if mid < 0 || mid > N-1 {
+			break
+		}
 		if target < nums[mid] {
 			high = mid - 1
 		} else if target > nums[mid] {
--- searching/searching_test.go
+++ searching/searching_test.go
@@ -22,3 +22,22 @@ func TestBinarySearch(t *testing.T) {
 		}
 	}
 }
+
+func TestInterpolationSearch(t *testing.T) {
+	tests := []struct {
+		input01 []int
+		input02 int
+		want    int
+	}{
+		{[]int{0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99}, 24, 3},   // 靠前
+		{[]int{0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99}, 62, 7},   // 靠后
+		{[]int{0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99}, 0, 0},    // 首
+		{[]int{0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99}, 99, 10},  // 尾
+		{[]int{0, 1, 16, 24, 35, 47, 59, 62, 73, 88, 99}, 999, -1}, // 无
+	}
+	for _, test := range tests {
+		if got := InterpolationSearch(test.input01, test.input02); test.want != got {
+			t.Errorf("InterpolationSearch(%v, %v) = %v, wanted: %v", test.input01, test.input02, got, test.want)
+		}
+	}
+}
